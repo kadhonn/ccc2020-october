89b5bfb4c1c5994895a552f18d9ac5be88f18b5c
Matthias Ableidinger <matthias.ableidinger@dynatrace.com>
2020-10-30 14:28:21 +0100

    wat

---
+++ console.sql
@@ -0,0 +1,41 @@
+do $$
+begin
+   for counter in 1..5 loop
+	raise notice 'counter: %', counter;
+   end loop;
+end; $$
+
+
+CREATE OR REPLACE FUNCTION somefunc(integer, text) RETURNS integer
+AS '
+    for counter in 1..5
+        loop
+            raise notice ''counter: %'', counter;
+        end loop;
+'
+LANGUAGE plpgsql;
+
+
+
+CREATE FUNCTION somefunc() RETURNS integer AS $$
+<< outerblock >>
+DECLARE
+    quantity integer := 30;
+BEGIN
+    RAISE NOTICE 'Quantity here is %', quantity;  -- Prints 30
+    quantity := 50;
+    --
+    -- Create a subblock
+    --
+    DECLARE
+        quantity integer := 80;
+    BEGIN
+        RAISE NOTICE 'Quantity here is %', quantity;  -- Prints 80
+        RAISE NOTICE 'Outer quantity here is %', outerblock.quantity;  -- Prints 50
+    END;
+
+    RAISE NOTICE 'Quantity here is %', quantity;  -- Prints 50
+
+    RETURN quantity;
+END;
+$$ LANGUAGE plpgsql;
