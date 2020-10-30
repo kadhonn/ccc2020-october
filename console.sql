a12d43ae43ee45380cbe17e7c3508967b0653bfe
Matthias Ableidinger <matthias.ableidinger@dynatrace.com>
2020-10-30 18:07:38 +0100

    lvl2

--- console.sql
+++ console.sql
@@ -1,14 +1,14 @@
 truncate table prices;
--- truncate table output;
-drop sequence serial;
-CREATE SEQUENCE serial START 1;
+truncate table tasks;
+truncate table task_costs;
+truncate table output;
 
 create or replace function readInput()
 returns text as $$
 declare
     tosplit text;
 begin
-    tosplit = pg_read_file('in/lvl1/level1_5.in');
+    tosplit = pg_read_file('in/lvl2/level2_5.in');
 --     raise notice '%', tosplit;
     return tosplit;
 end; $$ LANGUAGE plpgsql;
@@ -17,25 +17,80 @@ end; $$ LANGUAGE plpgsql;
 create or replace function parseInput()
 returns text as $$
 declare
-    current int;
+    i int := 1;
     price int;
     tosplit text;
+    task text;
+    taskid int;
+    taskduration int;
     n int;
+    m int;
 begin
     tosplit := readInput();
-    raise notice 'wat % ',tosplit;
-    n := SPLIT_PART(tosplit, E'\n', 1)::int;
-    raise notice 'the fuck %', n;
+    n := SPLIT_PART(tosplit, E'\n', i)::int;
+    raise notice 'before prices';
     loop
-        current := nextval('serial');
-        if (n = current ) then
+        i := i+1;
+        if (n+2 = i ) then
             exit;
         end if;
-        price := SPLIT_PART(tosplit, E'\n', current+1)::int;
-        insert into prices values (price, current);
+        price := SPLIT_PART(tosplit, E'\n', i)::int;
+        insert into prices values (price, i-2);
+    end loop;
+    m := SPLIT_PART(tosplit, E'\n', i)::int;
+    raise notice 'before tasks %',m;
+    loop
+        i := i+1;
+        if (m+n+3 = i ) then
+            exit;
+        end if;
+        task := SPLIT_PART(tosplit, E'\n', i);
+        taskid := SPLIT_PART(task, ' ', 1)::int;
+        taskduration := SPLIT_PART(task, ' ', 2)::int;
+        insert into tasks values (taskid, taskduration);
     end loop;
     return tosplit;
 end; $$ LANGUAGE plpgsql;
 
 select parseInput();
-select id-1, price from prices where price = any(select min(price) from prices);
+
+
+create or replace function calcCosts()
+returns text as $$
+declare
+    task tasks%ROWTYPE;
+    i int;
+begin
+    for task in select * from tasks loop
+        i := 0;
+        loop
+            if ( i + task.taskduration > (select count(*) from prices) ) then
+                exit;
+            end if;
+            insert into task_costs values( task.taskid,(select sum(price) from prices where id >= i and id <i+task.taskduration),i);
+            i := i+1;
+        end loop;
+    end loop;
+    return 0;
+end; $$ LANGUAGE plpgsql;
+
+create or replace function findMin()
+returns text as $$
+declare
+    out text;
+    mintask task_costs%ROWTYPE;
+begin
+    out = '' || ((select count(*) from tasks)::text) || E'\n';
+    for mintask in (select taskid, price, beginwtf from task_costs where taskid::text||price::text in (select taskid::text||min(price)::text from task_costs group by taskid)) loop
+
+        raise notice '%', mintask;
+        out = out || mintask.taskid || ' ' || mintask.beginwtf || E'\n';
+    end loop;
+    insert into output values (out);
+    return out;
+end; $$ LANGUAGE plpgsql;
+
+select calcCosts();
+select findMin();
+
+COPY (SELECT * from output) TO '/in/some_file_name' CSV QUOTE ' ';
