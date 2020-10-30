0a209df491ff2fdb3d3d4b1d0fbda31f594de8a1
Matthias Ableidinger <matthias.ableidinger@dynatrace.com>
2020-10-30 15:55:29 +0100

    lvl1

--- console.sql
+++ console.sql
@@ -1,11 +1,41 @@
-create or replace function splitinput()
-returns int as $$
+truncate table prices;
+-- truncate table output;
+drop sequence serial;
+CREATE SEQUENCE serial START 1;
+
+create or replace function readInput()
+returns text as $$
 declare
     tosplit text;
 begin
-    tosplit = pg_read_file('in/lvl1/level1_1.in');
-    raise notice '%', tosplit;
-    return 0;
+    tosplit = pg_read_file('in/lvl1/level1_5.in');
+--     raise notice '%', tosplit;
+    return tosplit;
 end; $$ LANGUAGE plpgsql;
 
-select splitinput();
+
+create or replace function parseInput()
+returns text as $$
+declare
+    current int;
+    price int;
+    tosplit text;
+    n int;
+begin
+    tosplit := readInput();
+    raise notice 'wat % ',tosplit;
+    n := SPLIT_PART(tosplit, E'\n', 1)::int;
+    raise notice 'the fuck %', n;
+    loop
+        current := nextval('serial');
+        if (n = current ) then
+            exit;
+        end if;
+        price := SPLIT_PART(tosplit, E'\n', current+1)::int;
+        insert into prices values (price, current);
+    end loop;
+    return tosplit;
+end; $$ LANGUAGE plpgsql;
+
+select parseInput();
+select id-1, price from prices where price = any(select min(price) from prices);
